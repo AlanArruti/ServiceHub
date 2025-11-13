45f9912f32736b1cf8155b370d0a11827cea06bd
iarablandi316 <iarablandiutn@gmail.com>
2025-11-12 22:05:25 -0300

    nuevas interfaces

---
+++ src/Clases/InterfazGeneral.java
@@ -0,0 +1,19 @@
+package Clases;
+
+import java.util.Scanner;
+
+public class InterfazGeneral {
+
+    public void interfazBinaria()
+    {
+        Scanner sc = new Scanner(System.in);
+        char seguir = 's';
+        int opcion;
+
+        while (seguir == 's')
+        {
+
+        }
+
+    }
+}
