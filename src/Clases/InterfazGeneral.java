05c52355dfbfe43f185db7e2729dc67a8844bed9
AlanArruti <arrutialan@gmail.com>
2025-11-13 18:44:27 -0300

    Update InterfazGeneral.java

--- src/Clases/InterfazGeneral.java
+++ src/Clases/InterfazGeneral.java
@@ -3,29 +3,5 @@ package Clases;
 import java.util.Scanner;
 
 public class InterfazGeneral {
-
-    public void interfazBinaria()
-    {
-        Scanner sc = new Scanner(System.in);
-        char seguir = 's';
-        int opcion;
-        InterfazCliente interCliente = new InterfazCliente();
-        InterfazEmpleado interEmpleado = new InterfazEmpleado();
-
-        while (seguir == 's')
-        {
-            System.out.println("¿Como desea registrarse?");
-            System.out.println("1 - Cliente. \n2 - Empleado. ");
-            opcion = sc.nextInt();
-
-            switch (opcion)
-            {
-                case 1:{
-                    Cliente cliente1 = interCliente.crearUsuario();
-                   // interCliente.verInfoOficio(cliente1);
-                }
-            }
-        }
-
-    }
+    
 }
