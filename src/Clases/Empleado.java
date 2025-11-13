43ca2053cfea11f9f4faea590e10fd14a5799e4a
29frn <viggonzalo93@gmail.com>
2025-11-12 21:48:04 -0300

    ...

--- src/Clases/Empleado.java
+++ src/Clases/Empleado.java
@@ -120,10 +120,6 @@ public class Empleado extends Persona implements Registrable {
         actualizarReputacion();
         System.out.println("Valoración registrada correctamente.");
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 9c14e5207d8c28f16c9b13ee987d3346fa11bcc9
 
     //Registrar accion del empleado
     @Override
